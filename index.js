b7a2d489a1a962baa6fae8e5e1ce557c309d3ee9
josmas <josmasflores@gmail.com>
2023-02-08 15:11:00 -0800

    adds weather

--- index.js
+++ index.js
@@ -55,6 +55,26 @@ app.get('/search', (req, res) => {
       });
   });
 
+
+const weatherApiKey = '7871668586efd0a7797494c778e548ac';
+app.get('/weather', (req, res) => {
+  const {all} = req.query
+  const theSplit = all.split(":")
+
+  const search_API_URL = 
+    `https://api.openweathermap.org/data/2.5/weather?lat=${theSplit[0]}&lon=${theSplit[1]}&appid=7871668586efd0a7797494c778e548ac`;
+  console.log(search_API_URL)
+
+  axios.get(search_API_URL)
+      .then(response => {
+        res.json(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send('Error fetching data');
+      });
+})
+
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
